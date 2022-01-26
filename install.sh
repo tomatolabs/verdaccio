c9fc437cad47d50a1c9fb5e8f88076928bc85ca2
henryleu <henryleu@126.com>
2022-01-26 11:33:04 +0800

    update

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
+# install nodejs
 curl -sL https://deb.nodesource.com/setup_current.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
+# install verdaccio
 npm config set registry https://registry.npm.taobao.org
-npm i -g yarn
-yarn config set registry https://registry.npm.taobao.org
-yarn global add sinopia
+sudo npm i -g verdaccio@latest
